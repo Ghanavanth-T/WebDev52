fc461064ba0755493a143ad23ff56a3c2afcf46f
BhuvanShridhar <bhuvanptr@gmail.com>
2023-12-09 17:18:50 +0530

    bg on click

---
+++ webdev-app/src/components/CSE/DigitalClock.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const DigitalClock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const hours = currentTime.getHours().toString().padStart(2, '0');
+  const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+  const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+
+  return (
+    <div>
+      <p>{`${hours}:${minutes}:${seconds}`}</p>
+    </div>
+  );
+};
+
+export default DigitalClock;
--- webdev-app/src/components/Course.jsx
+++ webdev-app/src/components/Course.jsx
@@ -1,69 +1,111 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// Based on the role given do it. Don't change it unless you are not assigned
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const CourseButton = ({ label, imageSrc, to, onClick, isZoomed }) => (
+  <Link to={to}>
+    <button
+      className={`text-white ${isZoomed ? 'transform scale-110 transition-transform duration-300' : ''} bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
+      onClick={onClick}
+    >
+      <img
+        className={`h-20 ${isZoomed ? 'scale-110 transition-transform duration-300' : ''}`}
+        src={imageSrc}
+        alt=""
+      />
+      <hr />
+      <strong>{label}</strong>
+    </button>
+  </Link>
+);
+
 const Course = () => {
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const handleZoomToggle = () => {
+    setIsZoomed(!isZoomed);
+  };
+
   return (
     <>
-    <div className='pt-12'>
-      <div className=' pl-16 font-bold font-serif'>
-        <h1 className='text-5xl'>Choose Your Course</h1>
-    </div>
-    </div>
-    <div className='flex justify-between pl-20 pr-20 pt-16 '>
-    <Link to='/CSE'>
-    <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-          <img className="h-20" src="/src/assets/books.png" alt="" />
-          <hr />
-          <button><strong>CSE</strong></button>
-        </button>
-      </Link>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20 flex pl-5" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>MECHANICAL</strong></button>
-      </button>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20 flex pl-5" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-      </button>
-    </div>
-    <div className='flex justify-between pl-48 pr-52 pt-16 '>
-    <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-    </button>
-    <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-    </button>
-    <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-    </button>
-    </div>
-    <div className='flex justify-between pl-20 pr-20 pt-16 '>
-    <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>CSE</strong></button>
-      </button>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>MECHANICAL</strong></button>
-      </button>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-      <img className="h-20" src="/src/assets/books.png" alt="" />
-      <hr />
-      <button><strong>ELECTRICAL</strong></button>
-      </button>
-    </div>
-    </>
-  )
-}
+      <div className="pt-12">
+        <div className="pl-16 font-bold font-serif">
+          <h1 className="text-5xl">Choose Your Course</h1>
+        </div>
+      </div>
 
-export default Course
+      <div className="flex justify-between pl-20 pr-20 pt-16">
+        <CourseButton
+          label="ELECTRICAL"
+          imageSrc="/src/assets/books.png"
+          to="/electrical"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="CSE"
+          imageSrc="/src/assets/books.png"
+          to="/CSE"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="MECHANICAL"
+          imageSrc="/src/assets/books.png"
+          to="/mechanical"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+      </div>
+
+      <div className="flex justify-between pl-48 pr-52 pt-16">
+        <CourseButton
+          label="CIVIL"
+          imageSrc="/src/assets/books.png"
+          to="/civil"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="CHEMICAL"
+          imageSrc="/src/assets/books.png"
+          to="/chemical"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="AEROSPACE"
+          imageSrc="/src/assets/books.png"
+          to="/aerospace"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+      </div>
+
+      <div className="flex justify-between pl-20 pr-20 pt-16">
+        <CourseButton
+          label="COMPUTER NETWORKS"
+          imageSrc="/src/assets/books.png"
+          to="/computer-networks"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="SOFTWARE ENGINEERING"
+          imageSrc="/src/assets/books.png"
+          to="/software-engineering"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+        <CourseButton
+          label="DATA STRUCTURES"
+          imageSrc="/src/assets/books.png"
+          to="/data-structures"
+          onClick={handleZoomToggle}
+          isZoomed={isZoomed}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Course;
