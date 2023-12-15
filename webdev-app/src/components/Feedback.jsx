9a68300496f3197bb9ee9a5018e62179d35bcbfa
BhuvanShridhar <bhuvanptr@gmail.com>
2023-12-15 23:26:14 +0530

    Loading spin,Upload files

--- webdev-app/src/components/Feedback.jsx
+++ webdev-app/src/components/Feedback.jsx
@@ -1,73 +1,89 @@
 import React, { useState } from 'react';
 
-const FeedbackForm = ({ onSubmit }) => {
+const LoadingSpinner = () => (
+  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+    </svg>
+    <span className="sr-only">Loading...</span>
+  </div>
+);
+
+const FeedbackForm = ({ onSubmit, onReset }) => {
   const [feedback, setFeedback] = useState('');
+  const [submitting, setSubmitting] = useState(false);
 
   const handleFeedbackChange = (event) => {
     setFeedback(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    setSubmitting(true);
+
+    // Simulate an asynchronous submission (e.g., API call)
+    // You can replace this with your actual submission logic
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     onSubmit(feedback);
+
+    setSubmitting(false);
   };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      {submitting && <LoadingSpinner />}
       <form onSubmit={handleSubmit} style={{ display: 'inline-block' }}>
         <label style={{ fontSize: '43px' }}>
           Feedback:
           <br />
-          
-
-          <label for="message" class="block mb-2 text-sm font-medium text-white dark:text-black"></label>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-
-
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-white dark:text-black"></label>
+          <textarea
+            id="message"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your thoughts here..."
+            value={feedback}
+            onChange={handleFeedbackChange}
+          ></textarea>
         </label>
         <br />
-        <button type="submit" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ fontSize: '18px', marginTop: '10px' }}>
-          Submit Feedback
+        <div className="flex items-center justify-center w-full">
+          <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+              </svg>
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            </div>
+            <input id="dropzone-file" type="file" className="hidden" />
+          </label>
+        </div>
+        <br />
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          style={{ fontSize: '18px', marginTop: '10px' }}
+          disabled={submitting}
+        >
+          {submitting ? 'Submitting...' : 'Submit Feedback'}
         </button>
+        {onReset && (
+          <button
+            type="button"
+            onClick={onReset}
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            style={{ fontSize: '18px', marginTop: '10px' }}
+            disabled={submitting}
+          >
+            Reset
+          </button>
+        )}
       </form>
     </div>
   );
 };
 
-const ThankYouMessage = ({ onReset }) => (
-  <div style={{ fontSize: '40px', marginTop: '30px' }}>
- <p>Thank you for your feedback!</p>
-    <p>Your feedback will be reviewed.</p> 
-    
-    <button onClick={onReset} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ fontSize: '18px', marginTop: '10px' }}>
-      Reset
-    </button>
-  </div>
-);
-
-
-const Feedback = () => {
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleFormSubmit = (feedback) => {
-    // You can handle the submission logic here (e.g., send feedback to a server).
-    // For simplicity, this example just sets the 'submitted' state to true.
-    setSubmitted(true);
-  };
-
-  const handleReset = () => {
-    setSubmitted(false);
-  };
-
-  return (
-    <div style={{ textAlign: 'center', fontSize: '18px' }}>
-      {submitted ? (
-        <ThankYouMessage onReset={handleReset} />
-      ) : (
-        <FeedbackForm onSubmit={handleFormSubmit} />
-      )}
-    </div>
-  );
-};
-
-export default Feedback;
+export default FeedbackForm;
